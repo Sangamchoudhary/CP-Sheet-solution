b0b1cf07cae8a2bc1dbd275a3b8bb3e074407351
Sangam choudhary <Sangamchoudhary7@gmail.com>
2022-09-01 21:23:40 +0530

    CP

---
+++ Template.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+// ---------------------------------- SANGAM CHOUDHARY ---------------------------------------
+typedef long long int ll;
+typedef pair<long long int,long long int> pll;
+#define tpl tuple<ll,ll,ll>
+#define M416 ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define mod 1000000007
+#define vll vector<ll>
+#define vpl vector<pll>
+#define vtl vector<tuple<ll,ll,ll>>
+#define ump(x,y) unordered_map<x,y>
+#define ust(x) unordered_set<x>
+#define pb push_back
+#define ppb pop_back
+#define inp(arr) for(auto &it : arr) cin>>it
+#define mem(arr) memset(arr,-1,sizeof arr)
+#define endl "\n"
+#define rep(x,y,z) for(ll x=y;x<z;x++)
+#define rev(x,y,z) for(ll x=y;x>=0;x--)
+#define all(v) v.begin(),v.end()
+#define next cout<<endl
+#define maxi(arr) *max_element(all(arr))
+#define mini(arr) *max_element(all(arr))
+void print(ll val){cout<<val<<" ";}
+void print(vector<ll> &arr) {for(auto &i : arr) cout<<i<<" "; next;}
+void print(string str) {cout<<str<<" ";}
+// -----------------------------------------------------------------------------------
+
+void solve(){
+    ll n; cin>>n;
+    vll arr(n); inp(arr);
+    
+}
+ 
+int main() {
+    M416
+	int t = 1; 
+	// cin>>t;
+	while(t--) solve();
+}
