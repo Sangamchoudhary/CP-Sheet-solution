a470b792ba758b2db4328ea06c050df1fdfbfb9b
Sangam choudhary <Sangamchoudhary7@gmail.com>
2022-09-14 00:42:18 +0530

    CP

--- Template.cpp
+++ Template.cpp
@@ -3,9 +3,10 @@ using namespace std;
 // ---------------------------------- SANGAM CHOUDHARY ---------------------------------------
 typedef long long int ll;
 typedef pair<long long int,long long int> pll;
+#define ff first
+#define ss second
 #define tpl tuple<ll,ll,ll>
 #define quad tpl<ll,ll,pll>
-#define M416 ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define mod 1000000007
 #define vll vector<ll>
 #define vpl vector<pll>
@@ -21,33 +22,28 @@ typedef pair<long long int,long long int> pll;
 #define rev(x,y,z) for(ll x=y;x>=0;x--)
 #define all(v) v.begin(),v.end()
 #define nl cout<<endl
-#define maxi(arr) *max_element(all(arr))
-#define mini(arr) *max_element(all(arr))
-int dx[] = {0,0,-1,1,-1,-1,1,1};
-int dy[] = {1,-1,0,0,-1,1,1,-1};
-void print(char val){cout<<val<<" ";}
-void print(ll val){cout<<val<<" ";}
+#define findMax(arr) *max_element(all(arr))
+#define findMin(arr) *min_element(all(arr))
+#define findSum(arr) accumulate(all(arr),0ll)
+bool isPrime(ll n) {for(ll i=2;i<=sqrt(n);i++) if(n % i == 0) return false; return true;}
+void print(char x) {cout<<x<<" ";}
+void print(ll x) {cout<<x<<" ";}
 void print(vector<ll> &arr) {for(auto &i : arr) cout<<i<<" "; nl;}
-void print(string str) {cout<<str<<" ";}
+void print(string str) {cout<<str;}
 /*
 Hint:- 
 - Read highlighted text 
 - Think of reverse approach
 - Make valid jumps only
-- Question is wearing a mask - decode it
-- Greedy, Binary_search
+- State optimization not possible? then think greedy
 */
-// -----------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------
 
 void solve(){
-    ll n; cin>>n;
-    vll a(n); inp(a);
     
 }
- 
 int main() {
-    M416
-    int t = 1; 
-    cin>>t;
-    while(t--) solve();
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); int t = 1; 
+    // cin>>t;
+    while(t--) solve(), nl;
 }
